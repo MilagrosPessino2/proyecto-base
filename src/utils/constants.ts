e70a8c08a9a57d08030bed33523e799b3ecc9a9d
MilagrosPessino2 <pessinomilagros@gmail.com>
2025-08-29 15:54:02 -0300

    Algunos cambios

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,11 @@ export const Roles = {
   ADMINISTRADORES: "ADMINISTRADORES",
   OTRO: "OTRO"
 };
+
+export const Imagenes = {
+  JPG: "JPG",
+  PNG: "PNG",
+  JPEG: "JPEG",
+  GIF: "GIF",
+  SVG: "SVG"
+};
